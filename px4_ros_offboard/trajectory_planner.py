19c9c40b0b4d1e34f9fc65e476b492d61a64ff31
atarbabgei <atarbabgei@gmail.com>
2024-09-24 08:49:57 +0100

    add more trajectories and specify offset

--- px4_ros_offboard/trajectory_planner.py
+++ px4_ros_offboard/trajectory_planner.py
@@ -40,14 +40,14 @@ class TrajectoryPlanner(Node):
         self.current_state = "IDLE"
         self.last_state = self.current_state
 
-        self.height_offset = 0.00  # Offset to add to the z position
+        self.height_offset = 0.0  # Offset to add to the z position
 
         self.start_x = 2.2
         self.start_y = 0.0
         self.start_z = 1.4  # Altitude to takeoff to 3.0
 
         # Load trajectory data from CSV
-        data = pd.read_csv('/home/atar/rolling_drone_ws/src/px4_ros_offboard/config/trajectory_reduced_20.csv')
+        data = pd.read_csv('/home/atar/rolling_drone_ws/src/px4_ros_offboard/config/trajectory_demo5_epi4.csv')
         x = data['Drone_X']
         y = data['Drone_Y']
         z = data['Drone_Z'] - self.height_offset  # Adjust z with height offset
